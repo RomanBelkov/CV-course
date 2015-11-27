073c1c3e45fda06d3a9c81687fbb149014ed2b6a
Roman Belkov <romanbelkov@gmail.com>
2015-11-27 16:49:29 +0300

    AR: Square recognition

--- AR/1.py
+++ AR/1.py
@@ -1,11 +1,30 @@
 import numpy as np
 import cv2
 
-video_capture = cv2.VideoCapture(0)
 
-#if video_capture.isOpened():  #  check if we succeeded
-#    -1
+# helping function for find_squares. Taken from OpenCV samples gallery
+def angle_cos(p0, p1, p2):
+    d1, d2 = (p0 - p1).astype('float'), (p2 - p1).astype('float')
+    return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1, d1) * np.dot(d2, d2)))
+
+
+# routine for finding squares on image. Taken from OpenCV samples gallery
+def find_squares(contours):
+    squares = []
+    for cnt in contours:
+        cnt_len = cv2.arcLength(cnt, True)
+        cnt = cv2.approxPolyDP(cnt, 0.02 * cnt_len, True)
+        if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+            cnt = cnt.reshape(-1, 2)
+            max_cos = np.max([angle_cos(cnt[i], cnt[(i + 1) % 4], cnt[(i + 2) % 4]) for i in xrange(4)])
+            if max_cos < 0.1:
+                squares.append(cnt)
+    return squares
+
+
 red = (0, 0, 255)
+
+video_capture = cv2.VideoCapture()
 
 while True:
     # Capture frame-by-frame
@@ -19,7 +38,9 @@ while True:
 
     contours, hierarchy = cv2.findContours(threshold_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
-    cv2.drawContours(frame, contours, -1, red, 2)
+    square_contours = find_squares(contours)
+
+    cv2.drawContours(frame, square_contours, -1, red, 2)
 
     cv2.imshow('Video', frame)
 

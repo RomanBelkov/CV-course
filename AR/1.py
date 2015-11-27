1e5149478b3fb135ef5782c2df8c4d39c643d5b9
Roman Belkov <romanbelkov@gmail.com>
2015-11-27 13:44:45 +0300

    AR: Added thresholding using Otsu's binarization

--- AR/1.py
+++ AR/1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 
-
 video_capture = cv2.VideoCapture(0)
 
 #if video_capture.isOpened():  #  check if we succeeded
@@ -10,7 +9,12 @@ video_capture = cv2.VideoCapture(0)
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
-    cv2.imshow('Video', frame)
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    ret, threshold_image = cv2.threshold(gray_frame, 128.0, 255.0, cv2.THRESH_OTSU)
+
+    cv2.imshow('Video', threshold_image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break

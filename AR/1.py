11795c9b2066169936fcedd1a08b6a327fafb3a0
Roman Belkov <romanbelkov@gmail.com>
2015-11-27 15:08:10 +0300

    AR: Drawing contours on image

--- AR/1.py
+++ AR/1.py
@@ -5,6 +5,7 @@ video_capture = cv2.VideoCapture(0)
 
 #if video_capture.isOpened():  #  check if we succeeded
 #    -1
+red = (0, 0, 255)
 
 while True:
     # Capture frame-by-frame
@@ -12,9 +13,15 @@ while True:
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    ret, threshold_image = cv2.threshold(gray_frame, 128.0, 255.0, cv2.THRESH_OTSU)
+    blurred_gray_image = cv2.blur(gray_frame, (5, 5))
 
-    cv2.imshow('Video', threshold_image)
+    ret, threshold_image = cv2.threshold(blurred_gray_image, 128.0, 255.0, cv2.THRESH_OTSU)
+
+    contours, hierarchy = cv2.findContours(threshold_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    cv2.drawContours(frame, contours, -1, red, 2)
+
+    cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
